fec1025f86db3bb9ec79a2c48a5e9c04fa349b67
edsandorf <edsandorf@gmail.com>
2020-03-26 08:23:44 +0000

    Updated copy-dotfiels

--- copy-dotfiles.sh
+++ copy-dotfiles.sh
@@ -13,7 +13,30 @@ cd ~
 git clone https://github.com/edsandorf/.dotfiles.git
 cd ~/.dotfiles
 
+# git
+ln -s .gitconfig ~/.gitconfig
+
+# Conky
+ln -s .conkyrc ~/.conkyrc
+
+# .bashrc (not really necessary now that I am using fish)
+ln -s .bashrc ~/.bashrc
+
+# Config atom
+rm ~/.atom/config.cson
+ln -s config.cson ~/.atom/config.cson
+
 # Fish shell config
 ln -s .config.fish ~/.config/fish/config.fish
 
+# ccache
+mkdir ~/.ccache
+ln -s ccache.conf ~/.ccache/ccache.conf
+
 # Makevars
+mkdir ~/.R
+ln -s Makevars ~/.R/Makevars
+
+# TeXStudio
+rm ~/.config/texstudio/texstudio.ini
+ln -s texstudio.ini ~/.config/texstudio/texstudio.ini
