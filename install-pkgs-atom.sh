ff4a03b8dcb097637efa880d280bdf1c714133c0
edsandorf <edsandorf@gmail.com>
2020-03-27 09:18:16 +0000

    Added atom packages

--- install-pkgs-atom.sh
+++ install-pkgs-atom.sh
@@ -6,3 +6,7 @@
 # Author:
 #   Erlend Dancke Sandorf <edsandorf [:at:] gmail.com>
 #
+apm install atom-ide-ui ide-r ide-python ide-rust atom-language-r \
+language-liquid hydrogen hydrogen-launcher gpp-compiler minimap \
+pigments platformio-ide-terminal atom-material-ui atom-material-syntax \
+file-icons project-manager
