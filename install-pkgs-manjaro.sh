01169d170a247dd825121a70b7d339c53b7e8bf4
edsandorf <edsandorf@gmail.com>
2020-03-30 09:39:45 +0100

    Added -y flag to yay

--- install-pkgs-manjaro.sh
+++ install-pkgs-manjaro.sh
@@ -14,30 +14,30 @@
 #   https://pat-s.me/post/arch-install-guide-for-r/
 
 # Utility packages
-yay -Su fish neofetch chromium ranger zotero atom insync dropbox \
+yay -Syu fish neofetch chromium ranger zotero atom insync dropbox \
 conky git-lfs gitg pdftk qpdf tldr ccache jdk jupyter downgrade \
 ranger firewalld xclip
 
 # Python packages
-yay -Su python-gdal python-yaml python-jinja python-psycopg2 python-owslib \
+yay -Syu python-gdal python-yaml python-jinja python-psycopg2 python-owslib \
 python-numpy python-pygments
 
 # Important libraries for spatial applications
-yay -Su gdal udunits postgis jdk-openjdk openjdk-src
+yay -Syu gdal udunits postgis jdk-openjdk openjdk-src
 
 # Texlive and pandoc
-yay -Su texlive-core texstudio pandoc pandoc-citeproc pandoc-crossref
+yay -Syu texlive-core texstudio pandoc pandoc-citeproc pandoc-crossref
 
 # R
-yay -Su libopenblas r rstudio-desktop-bin radian
+yay -Syu libopenblas r rstudio-desktop-bin radian
 
 # Julia
-yay -Su julia
+yay -Syu julia
 
 # Packages enabling spatial analysis
-yay -Su jq gcc-fortran tk nlopt gsl v8-3.14
+yay -Syu jq gcc-fortran tk nlopt gsl v8-3.14
 
 # Other Packages
-yay -Su spotify audacity blender gimp deluge obs-studio virtualbox
+yay -Syu spotify audacity blender gimp deluge obs-studio virtualbox
 
 sudo pacman -R firefox kate
