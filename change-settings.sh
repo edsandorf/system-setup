13bee05f9db3081a36689fa57fc2a0f34b634e86
edsandorf <edsandorf@gmail.com>
2020-03-31 15:57:35 +0100

    Added username to chsh

--- change-settings.sh
+++ change-settings.sh
@@ -10,7 +10,7 @@
 #   https://pat-s.me/post/arch-install-guide-for-r/
 
 # Change the shell to fish shell
-sudo chsh -s `which fish`
+sudo chsh -s `which fish` edsandorf
 
 # Enable trim for optimizing SSD performance
 sudo systemctl enable fstrim.timer
