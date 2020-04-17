198383f342fbd24615cd498660917d1896b9fae1
edsandorf <edsandorf@gmail.com>
2020-04-17 10:40:56 +0100

    Changed to single command ensure executability

--- install-pkgs-r.sh
+++ install-pkgs-r.sh
@@ -8,29 +8,25 @@
 #   Erlend Dancke Sandorf <edsandorf [:at:] gmail.com>
 #
 
-# Set up a personal library
+# Set up a personal library, install packages and run IRkernel
 R -e "dir.create(Sys.getenv('R_LIBS_USER'), recursive = TRUE);
-      .libPaths(Sys.getenv('R_LIBS_USER'))"
-
-# Execute the following R command
-R -e "install.packages(
-  c(
-    'tidyverse', 'lobstr', 'pryr', 'rlang', 'progress', 'R6',
-    'dtplyr', 'dbplyr', 'crayon', 'bench', 'haven', 'lintr',
-    'devtools', 'rsconnect', 'roxygen2', 'testthat', 'feather',
-    'shiny', 'DT', 'shinyjs', 'shinyWidgets', 'janitor',
-    'knitr', 'komadown', 'bookdown', 'patchwork', 'pool', 'config',
-    'kableExtra',
-    'aws.s3', 'RMariaDB', 'data.table',
-    'maxLik', 'trustOptim', 'nloptr', 'numDeriv', 'ucminf',
-    'Rfast', 'matrixStats',
-    'apollo', 'gmnl', 'mlogit',
-    'randtoolbox', 'cartography', 'sf', 'AER', 'car', 'psych',
-    'xtable', 'microbenchmark', 'swirl', 'cli',
-    'IRkernel', 'languageserver'
-  ),
-  repos = 'http://cran.rstudio.com'
-)"
-
-# Install the Jupyter kernel for current user only
-R -e "IRkernel::installspec()"
+      .libPaths(Sys.getenv('R_LIBS_USER'));
+      install.packages(
+        c(
+          'tidyverse', 'lobstr', 'pryr', 'rlang', 'progress', 'R6',
+          'dtplyr', 'dbplyr', 'crayon', 'bench', 'haven', 'lintr',
+          'devtools', 'rsconnect', 'roxygen2', 'testthat', 'feather',
+          'shiny', 'DT', 'shinyjs', 'shinyWidgets', 'janitor',
+          'knitr', 'komadown', 'bookdown', 'patchwork', 'pool', 'config',
+          'kableExtra',
+          'aws.s3', 'RMariaDB', 'data.table',
+          'maxLik', 'trustOptim', 'nloptr', 'numDeriv', 'ucminf',
+          'Rfast', 'matrixStats',
+          'apollo', 'gmnl', 'mlogit',
+          'randtoolbox', 'cartography', 'sf', 'AER', 'car', 'psych',
+          'xtable', 'microbenchmark', 'swirl', 'cli',
+          'IRkernel', 'languageserver'
+        ),
+        repos = 'http://cran.rstudio.com'
+      );
+      IRkernel::installspec()"
