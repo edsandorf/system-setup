06d8121fd831b45a93001d0a55e2b5a4a02b9c9a
edsandorf <edsandorf@gmail.com>
2020-05-18 13:06:07 +0100

    Conky startup

--- conky-startup.sh
+++ conky-startup.sh
@@ -2,6 +2,7 @@
 #
 # Set to sleep for a short duration to allow desktop startup
 #
-# ~/.config/autostart/
+# Make executable: chmod +x conky-startup.sh
+# copy conky-startup.desktop -> .config/autostart/ and make it executable
 sleep 10
 conky -b &
