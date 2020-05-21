3ee78a8fbc01d9a297bf6b50e4c13ab0dea9486b
edsandorf <edsandorf@gmail.com>
2020-05-21 08:20:52 +0100

    Added alsa-tools

--- install-pkgs-manjaro.sh
+++ install-pkgs-manjaro.sh
@@ -18,7 +18,7 @@ sudo pacman -Syu --needed base-devel yay
 # Utility packages
 yay -Syu fish neofetch chromium ranger zotero atom insync dropbox \
 conky git-lfs gitg pdftk qpdf tldr ccache jdk jupyter downgrade \
-ranger xclip seahorse polybar
+ranger xclip seahorse polybar alsa-tools
 
 # Security and VPN
 yay -Syu firewalld wireguard
